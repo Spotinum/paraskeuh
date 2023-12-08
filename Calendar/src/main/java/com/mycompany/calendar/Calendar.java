dec659dc3b8dc6c6edf0547cda0e8ea4ac05ffa0
IOANNIS ROUMELIOTIS <it2021087@hua.gr>
2023-12-08 20:10:36 +0200

    First stage

---
+++ Calendar/src/main/java/com/mycompany/calendar/Calendar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calendar;
+
+/**
+ *
+ * @author giannis
+ */
+public class Calendar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
